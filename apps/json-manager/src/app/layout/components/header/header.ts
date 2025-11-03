d93a981e1c712babc733814f803804f82f28d1e2
Loïc Pennamen <loic.pennamen@gmail.com>
2025-11-03 13:02:42 +0100

    Adds header layout and base styles
    Implements a basic header layout with navigation links.
    Includes a logo, site title, and navigation using ng-bootstrap components and router links.
    Introduces base styles and global CSS variables.

--- apps/json-manager/src/app/layout/components/header/header.ts
+++ apps/json-manager/src/app/layout/components/header/header.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-header',
-  imports: [],
+  imports: [NgbNavModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.sass',
 })
